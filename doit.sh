7b2b11cc4701759d8388bad97d215ced743afe49
pgk <pkeir@users.noreply.github.com>
2014-10-04 19:42:19 +0100

    Added some useful screengrabs including the 4 arrow icons; and the green button icon. Also included code which examines, and for now, edits the images.

--- doit.sh
+++ doit.sh
@@ -8,9 +8,14 @@ DAPHNE="DAPHNE: First Ever Multiple Arcade Laserdisc Emulator =]"
 #XID=`xdotool search --limit 1 --name "sdq_grab"`   # Likely this directory.
 #XID=`xdotool search --limit 1 --name "scrot"`   # The scrot directory window. 
 #XID=`xdotool search --limit 1 --name "DAPHNE"`   # The scrot directory window. 
+#display images/sdq.png &
 XID=$(xdotool search --name "$DAPHNE")
+#XID=$(xdotool search --name "ImageMagick")
+#xdotool windowmap --sync $XID
 #echo $XID
 
 # g++ sdq_grab.cpp -lX11 -o sdq_grab
-./sdq_grab $XID
+./sdq_grep $XID
 
+#xdotool windowkill $XID
+#xdotool windowunmap --sync $XID
