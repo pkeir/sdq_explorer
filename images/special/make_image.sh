71ce86a8da519679948526083f2c08a2579a6a7d
Paul Keir <pkeir@users.noreply.github.com>
2015-04-19 19:40:36 +0100

    Added a script to use ImageMagick to concatenate images.

---
+++ images/special/make_image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+montage -mode concatenate -tile 7x \
+          totem_04_button_009050.png \
+       fire_woman_05_left_025500.png \
+  pyramid_steps_07_button_043850.png \
+         serpents_08_left_067100.png \
+            mummy_02_left_085300.png \
+          mummy_04_button_096750.png \
+           dragon_04_down_178350.png \
+           dragon_09_left_196200.png \
+            snake_02_left_258800.png \
+           snake_03_right_269850.png \
+       river_jump_04_left_305100.png \
+       windmill_04_button_419000.png \
+         windmill_05_left_431650.png \
+          witch_10_button_550500.png \
+  out.png
