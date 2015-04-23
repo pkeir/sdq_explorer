7b6ca041f3fc50beac77503db36cac2a017067f5
Paul Keir <paul.keir@uws.ac.uk>
2015-04-23 14:01:51 +0100

    New forward_list container initialised to store the ordered lists of all possible (156x5) moves.

--- sdq_explorer.hpp
+++ sdq_explorer.hpp
@@ -8,12 +8,10 @@
 #include <list>
 #include <chrono>
 #include <thread>
+#include <forward_list>
 #include <climits>
 #include <cassert>
 #include <Imlib2.h>
-
-// sudo apt-get install libpng-dev libfreetype6-dev libimlib2-dev
-// g++ -std=c++11 sdq_explorer.cpp -lX11 -lImlib2 -lxdo -o sdq_explorer
 
 extern "C" {   // xdo.h assumes a C compiler, so let's wrap it in extern "C"
 #include <xdo.h> // sudo apt-get install libxdo-dev
@@ -38,6 +36,9 @@ prompt_e find_prompt(const DATA32 *, const int, const int,
                      unsigned &, unsigned &);
 inline prompt_e find_secret_icon(const level_e, const prompt_e, const unsigned);
 void take_screenshot(const char *, const Imlib_Image &);
+
+void playthrough(bool = true);
+void exhaustive();
 
 // Pertinent data relating to a single Quick Time Event (QTE)
 struct qte_info {
